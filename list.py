d25e922461c6ca00f8ad8a50e91fd85caf1ff33c
yoobohou <59799359+yoobohou@users.noreply.github.com>
2024-10-20 16:37:17 +0100

    Update list.py

--- list.py
+++ list.py
@@ -22,3 +22,11 @@ bnewlist = [i for i in fruits if "b" in i]
 
 print(anewlist)
 print(bnewlist)
+
+
+
+numbers =[1, -3,3 , -6 , 7, 8, 9, -10, 11, -12, 13, 14, -15, 16, 17, 18, 19, 20]
+positivenumbers =[i for i in numbers if i>0]
+print(positivenumbers)
+negativenumbers =[i for i in numbers if i<0]
+print(negativenumbers)

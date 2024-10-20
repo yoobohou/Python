5179eaf787da1c229d6fb7e2880de9e1b6dffb20
yoobohou <59799359+yoobohou@users.noreply.github.com>
2024-10-20 16:02:43 +0100

    Update list.py

--- list.py
+++ list.py
@@ -1,5 +1,22 @@
 fruits = ["apple", "banana", "cherry", "date"]
 
+anewlist = [] 
+bnewlist = []
+
+for i in fruits:
+  
+  if "a" in i :
+    anewlist.append(i)
+  
+  if "b" in i:
+    bnewlist.append(i)
+
+print(anewlist) 
+print(bnewlist) 
+## using comprehensive list , this is how to create list from list if the condition is true 
+
+fruits = ["apple", "banana", "cherry", "date"]
+
 anewlist = [i for i in fruits if "a" in i]
 bnewlist = [i for i in fruits if "b" in i]
 
